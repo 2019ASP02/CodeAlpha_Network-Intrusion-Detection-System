076d1a7b12114da5fbdcbd18c50bb3adc950b579
2019ASP02 <149567036+2019ASP02@users.noreply.github.com>
2025-01-04 23:44:37 +0530

    define startsniffer function

--- nids.py
+++ nids.py
@@ -1,6 +1,13 @@
-from scapy import sniffer ,TCP,IP,UDP
+from scapy import sniff ,TCP,IP,UDP
 
 #Vulnerable ports  FTP, SSH, Telnet,SMTP,TCP, HTTP, HTTPS
 suspicious_ports = [21, 22, 23, 25, 80, 139, 443, 8080, 8443]
-max_package_size = 1500
+max_package_size = 1500  #
 alert_log = "nids_alerts.log"
+
+def detect_intrusion(packet):
+    
+
+def start_sniffer(interface = 'Wi-Fi'):
+    print(f"Starting packet sniffing on inferface: {interface}")
+    sniff(iface = interface, prn = detect_intrusion, store = False)
